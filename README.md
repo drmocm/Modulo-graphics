199c09b213d6afb50ab463709e6b6a93f28235c1
Marcus Metzler <mocm@mocm.de>
2018-12-21 09:33:15 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Modulo-graphics
 some fun with python
+
+This is just a little test on how to use the TK toolkit on python
+and having a little mathematic fun at the same time.
+
+We take a number (n) of equidistant point on a circle and number them 0 to n.
+Then we go around the circle and multiply the number of the point x with a
+number f, take the result modulo n and connect the point x with the point
+y = x*f mod n
+After we complete a circle we increase f. This result in a fun little sequence
+of patterns.
---
+++ modulo.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import tkinter as tk
+import math
+import time
+
+root = tk.Tk()
+w = 1920*1.5
+h = 1080*1.5
+r = h/2
+step = 1.0/25.0
+msec = 40
+np = 256
+plist=[[w/2+r,0]]
+
+def _draw_mod(self, f, width, height, **kwargs):
+    self.delete("all")
+    self.create_text(300,100,fill="white",font="Times 20 italic bold",text="Faktor "+"{:.2f}".format(f))
+    self.create_oval(width/2-r, height/2-r,width/2+r,height/2+r, fill="blue", outline="#DDD", width=4)
+    for n in range(1, np-1):
+        p = [r*math.cos(2*math.pi*n/np),r*math.sin(2*math.pi*n/np)]
+        q = [r*math.cos(2*math.pi*n*f/np),r*math.sin(2*math.pi*n*f/np)]
+        self.create_line(p[0]+width/2,p[1]+height/2,q[0]+width/2,q[1]+height/2, **kwargs)
+tk.Canvas.draw_mod = _draw_mod
+
+counter = 0
+def counter_mod(canvas):
+  counter = 0
+  def count():
+    global counter
+    counter += step
+    counter %= np
+    canvas.draw_mod(counter,w,h,fill="yellow")    
+    canvas.after(msec, count)
+  count()
+
+def counter_reset():
+    global counter
+    counter = 0
+  
+canvas = tk.Canvas(root, width=w, height=h, borderwidth=0, highlightthickness=0, bg="black")
+canvas.grid()
+
+counter_mod(canvas)
+
+button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+button.grid()
+button = tk.Button(root, text='Restart', width=25, command=counter_reset)
+button.grid()
+
+root.wm_title("mod")
+root.mainloop()
